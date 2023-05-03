ef86bab7946326c8eb5bd4fb8b59aa836a99f8bb
HyoJeong Moon <57999419+kot8585@users.noreply.github.com>
2023-05-03 18:22:03 +0900

    ellie - searchHeader  snapshot 테스트 추가

--- youtube-final/src/components/tests/SearchHeader.test.js
+++ youtube-final/src/components/tests/SearchHeader.test.js
@@ -1,28 +1,32 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Route, useLocation } from "react-router-dom";
 import { withRouter } from "../../tests/utils";
 import SearchHeader from "../SearchHeader";
 import { act } from "react-dom/test-utils";
+import renderer from "react-test-renderer";
 
 describe("SearchHeader", () => {
   const keyword = "testKeyword";
 
-  //이거 안돼...
-  it(
-    "렌더링할때 param에 keyword가 있으면 input의 value에 세팅한다.",
-    act(() => {
-      render(
-        withRouter(
-          <>
-            <Route path={`/${keyword}`} element={<SearchHeader />} />
-          </>
-        )
-      );
+  it("renders correctly", () => {
+    const component = renderer.create(
+      withRouter(<Route path="/" element={<SearchHeader />} />)
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
 
-      expect(screen.findByText(keyword)).toBeInTheDocument();
-    })
-  );
+  it("renders with keyword correctly", async () => {
+    render(
+      withRouter(
+        <Route path="/:keyword" element={<SearchHeader />} />,
+        `/${keyword}`
+      )
+    );
+    await waitFor(() => {
+      expect(screen.findByDisplayValue(keyword)).toBeInTheDocument();
+    });
+  });
 
   it("navigates to video page with keyword", async () => {
     function LocationStateDisplay() {
